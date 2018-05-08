eaad80046802ca6b518fd9356c8a3827f6004451
stephane de la torre <s.dlt@hotmail.fr>
2018-05-08 17:25:26 +0200

    session 2/5 done

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,9 @@
-import { renderComponent , expect } from '../test_helper';
-import App from '../../src/components/app';
+import { renderComponent, expect } from "../test_helper";
+import App from "../../src/components/app";
 
-describe('App' , () => {
-  let component;
-
-  beforeEach(() => {
-    component = renderComponent(App);
-  });
-
-  it('renders something', () => {
-    expect(component).to.exist;
+describe("App", () => {
+  it("Shows the correct text", () => {
+    const component = renderComponent(App);
+    expect(component).to.contain("React simple starter");
   });
 });
