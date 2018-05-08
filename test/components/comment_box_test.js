861384145343d3838697db15a5fbeedf30d23231
stephane de la torre <s.dlt@hotmail.fr>
2018-05-08 18:00:21 +0200

    session 2/8 done

---
+++ test/components/comment_box_test.js
@@ -0,0 +1,10 @@
+import { renderComponent, expect } from "../test_helper";
+import CommentBox from "../../src/components/comment_box";
+
+describe('CommentBox', () => {
+  it("Has a text area", () => {
+  });
+
+  it("Has a button", () => {
+  });
+});
