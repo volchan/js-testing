bd1434d3edf58f3ea80634cbe6e0ae68892d2e0f
stephane de la torre <s.dlt@hotmail.fr>
2018-05-08 18:09:58 +0200

    session 2/9 done

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -1,10 +1,11 @@
 import { renderComponent, expect } from "../test_helper";
 import CommentBox from "../../src/components/comment_box";
 
-describe('CommentBox', () => {
+describe("CommentBox", () => {
   it("Has a text area", () => {
+    const component = renderComponent(CommentBox);
+    expect(component.find('textarea')).to.exist;
   });
 
-  it("Has a button", () => {
-  });
+  it("Has a button", () => {});
 });
