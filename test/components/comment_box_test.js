44c30075d3a74722065b6d5e7b49246a275674df
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2018-05-09 18:30:50 +0200

    Update comment_box_test.js

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -1,35 +1,35 @@
 import { renderComponent, expect } from "../test_helper";
 import CommentBox from "../../src/components/comment_box";
 
-describe("CommentBox", () => {
+describe("CommentBox:", () => {
   let component;
 
   beforeEach(() => {
     component = renderComponent(CommentBox);
   });
 
-  it("Has the correct class", () => {
+  it("Has the correct class.", () => {
     expect(component).to.have.class("comment-box");
   });
 
-  it("Has a text area", () => {
+  it("Has a text area.", () => {
     expect(component.find("textarea")).to.exist;
   });
 
-  it("Has a button", () => {
+  it("Has a button.", () => {
     expect(component.find("button")).to.exist;
   });
 
-  describe("Entering some text", () => {
+  describe("Entering some text:", () => {
     beforeEach(() => {
       component.find("textarea").simulate("change", "new comment");
     });
 
-    it("Shows that text in the text area", () => {
+    it("Shows that text in the text area.", () => {
       expect(component.find("textarea")).to.have.value("new comment");
     });
 
-    it("When submitted, clears the input", () => {
+    it("When submitted, clears the input.", () => {
       component.simulate("submit");
       expect(component.find("textarea")).to.have.value("");
     });
