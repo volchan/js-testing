d57d79b5b4f7b2f5f33e07943e71695989c3cf7e
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2018-05-09 18:29:55 +0200

    Update app_test.js

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -8,11 +8,11 @@ describe("App", () => {
     component = renderComponent(App);
   });
 
-  it("Shows a comment box", () => {
+  it("Shows a comment box.", () => {
     expect(component.find(".comment-box")).to.exist;
   });
 
-  it("Shows a comment list", () => {
+  it("Shows a comment list.", () => {
     expect(component.find(".comment-list")).to.exist;
   });
 });
