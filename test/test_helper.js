036ac0726cef666c704d78198ab67c978d2e995d
stephane de la torre <s.dlt@hotmail.fr>
2018-05-10 18:05:30 +0200

    session 2/29 done

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,14 +1,26 @@
 import jsdom from "jsdom";
 import jquery from "jquery";
+import React from "react";
 import ReactDom from "react-dom";
 import TestUtils from "react-dom/test-utils";
+import { expect } from "chai";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+import reducers from "../src/reducers";
 
 global.document = jsdom.jsdom("<!doctype html><html><body></body></html>");
 global.window = global.document.defaultView;
 const $ = jquery(global.window);
 
 const renderComponent = ComponentClass => {
-  const componentInstance = TestUtils.renderIntoDocument(<ComponentClass />);
+  const componentInstance = TestUtils.renderIntoDocument(
+    <Provider store={createStore(reducers)}>
+      <ComponentClass />
+    </Provider>
+  );
 
   return $(ReactDom.findDOMNode(componentInstance));
 };
+
+export { renderComponent, expect };
