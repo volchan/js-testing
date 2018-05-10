2f961d2b46f35c36d71ccb3f5fbc5d5f09057d91
stephane de la torre <s.dlt@hotmail.fr>
2018-05-10 16:52:19 +0200

    fix app

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -16,6 +16,7 @@ class CommentBox extends Component {
 
   handleSubmit(event) {
     event.preventDefault();
+
     this.props.saveComment(this.state.comment);
     this.setState({ comment: "" });
   }
@@ -23,11 +24,14 @@ class CommentBox extends Component {
   render() {
     return (
       <form onSubmit={this.handleSubmit.bind(this)} className="comment-box">
+        <h4>Add a comment</h4>
         <textarea
           value={this.state.comment}
           onChange={this.handleChange.bind(this)}
         />
-        <button action="submit">Submit Comment</button>
+        <div>
+          <button action="submit">Submit Comment</button>
+        </div>
       </form>
     );
   }
--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -1,9 +1,10 @@
-import SAVE_COMMENT from "../actions/types";
+import { SAVE_COMMENT } from "../actions/types";
 
 export default (state = [], action) => {
   switch (action.type) {
     case SAVE_COMMENT:
-      return [ ...state, action.payload ];
+      return [...state, action.payload];
+    default:
+      return state;
   }
-  return state;
-};
+}
