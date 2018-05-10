a217151d3ed3e0e92a3ec63e9809f5c0baf48e60
stephane de la torre <s.dlt@hotmail.fr>
2018-05-10 17:49:08 +0200

    session 2/28 done

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,7 +1,14 @@
 import jsdom from "jsdom";
 import jquery from "jquery";
+import ReactDom from "react-dom";
+import TestUtils from "react-dom/test-utils";
 
 global.document = jsdom.jsdom("<!doctype html><html><body></body></html>");
 global.window = global.document.defaultView;
-
 const $ = jquery(global.window);
+
+const renderComponent = ComponentClass => {
+  const componentInstance = TestUtils.renderIntoDocument(<ComponentClass />);
+
+  return $(ReactDom.findDOMNode(componentInstance));
+};
